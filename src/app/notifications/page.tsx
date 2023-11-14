c571ebdfbdbc5e146c7c2aef1ba77ccd6309d55f
trithvik07 <rithvikthonukunuri@gmail.com>
2023-11-14 18:02:56 +0530

    Completed

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,24 +1,6 @@
 import Header from "../[Comps]/Header"
-import { getServerSession } from "next-auth/next"
 import NotificationFeed from "../[Comps]/NotificationFeed"
-export async function getProps() {
-    const session = await getServerSession()
-    if (!session) {
-        return {
-            redirect: {
-                destination: "/",
-                permanent: false
-            }
-        }
-    }
-    return {
-        props: {
-            session
-        }
-    }
-}
 const Notifications = async () => {
-    const props = getProps()
     return (
         <div>
             <Header label="Notifications" showBackArrow />
